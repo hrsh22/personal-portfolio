f54108990990fadfa3d22d44eb37b14311c3f9cc
Harsh Gupta <harsh.gupta2211@gmail.com>
2022-07-03 14:49:52 +0530

    updated contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import { useState } from "react";
 import {Container, Row, Col} from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
@@ -11,9 +12,25 @@ export const Contact = () => {
             message: ""
     }
 
-    const handleSubmit = () => {
-         
+    const handleSubmit = async (e) => {
+         e.preventDefault();
+         setButtonText('Sending...');
+         let response = await fetch("http://localhost:5000/contact",{
+             method: "POST",
+             headers: {
+                 "Content-Type": "Application/json;charset=utf-8",
+             },
+             body: JSON.stringify(formDetails),
+    });
+    setButtonText("Send");
+    let result = response.json();
+    setFormDetails(formInitialDetails);
+    if(result.code===200){
+        setStatus({success: true, message: 'Message sent successfully'});
+    } else {
+        setStatus({success: false, message: 'Something went wrong, please try again later.'});
     }
+}
     
     const [formDetails, setFormDetails] = useState(formInitialDetails);
     const [buttonText, setButtonText] = useState("Send");
