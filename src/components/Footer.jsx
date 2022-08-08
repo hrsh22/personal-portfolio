2b3228de813425859363c10d4a63abf97df84a4c
Harsh Gupta <harsh.gupta2211@gmail.com>
2022-08-09 01:07:14 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
+//import logo from "../assets/img/logo.svg";
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
@@ -9,10 +9,10 @@ export const Footer = () => {
         <footer className="footer">
             <Container>
                 <Row className="align-item-center">
-                    <Col sm={6}>
+                     {/* <Col sm={6}>
                         <img src={logo} alt="logo" />
-                    </Col>
-                    <Col sm={6} className="text-center text-sm-end">
+                    </Col> */}
+                    <Col className="text-center"> {/*text-sm-end - add this in className, sm={6} - add this after Col - when adding logo*/}
                         <div className="social-icon">
                             <a href="https://www.linkedin.com/in/hrsh22/" target="_blank" rel="noreferrer"><img src={navIcon1} alt="Linked IN"/></a>
                             <a href="https://github.com/hrsh22" target="_blank" rel="noreferrer"><img src={navIcon2} alt="GitHub"/></a>
