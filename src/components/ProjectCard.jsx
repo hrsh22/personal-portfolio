69cfc363fd35885ed4f363a1561d47f7c5e950fb
Harsh Gupta <harsh.gupta2211@gmail.com>
2022-08-09 01:02:10 +0530

    added links

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,13 +1,14 @@
 import {Col} from 'react-bootstrap';
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, link }) => {
     return(
         <Col size={12} sm={6} md={4}>
             <div className="proj-imgbx">
                 <img src={imgUrl} />
                 <div className="proj-txtx">
                     <h4>{title}</h4>
-                    <span>{description}</span>
+                    <span>{description}</span><br></br><br></br>
+                    <h4><a href={link} target="_blank"  rel="noreferrer">View Project</a></h4>
                 </div>
             </div>
         </Col>
