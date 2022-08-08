c527d200f55f501eb30f2e9aea4c52b2b6e2997b
Harsh Gupta <harsh.gupta2211@gmail.com>
2022-08-09 00:54:35 +0530

    updated new skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -33,25 +33,39 @@ export const Skills = () => {
                 <div className="row">
                     <div className="col-12">
                         <div className="skill-bx wow zoomIn">
-                            <h2>Skills</h2>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                            <h2>Skills</h2><br></br><br></br>
+                            {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p> */}
                             <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                                <div className="item">
+                            <div className="item">
                                     <img src={meter1} alt="Image" />
-                                    <h5>Web Development</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={meter2} alt="Image" />
-                                    <h5>Brand Identity</h5>
+                                    <h5>Python</h5>
                                 </div>
                                 <div className="item">
                                     <img src={meter3} alt="Image" />
-                                    <h5>Logo Design</h5>
+                                    <h5>React</h5>
+                                </div>
+                                
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>SQL</h5>
+                                </div>
+                                
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>HTML</h5>
                                 </div>
                                 <div className="item">
                                     <img src={meter1} alt="Image" />
-                                    <h5>Web Development</h5>
+                                    <h5>CSS</h5>
                                 </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image" />
+                                    <h5>C</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image" />
+                                    <h5>Javascript</h5>
+                                </div>                       
                             </Carousel>
                         </div>
                     </div>
